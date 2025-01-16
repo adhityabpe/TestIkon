2bc73154bb2393cc77fef273624f64cc5e3906d6
AdhityaBagusPE <adityaput14@gmail.com>
2025-01-16 19:49:00 +0700

    Finish Assign as request

--- src/main/java/com/testikon/controller/MainController.java
+++ src/main/java/com/testikon/controller/MainController.java
@@ -4,14 +4,14 @@ import com.testikon.config.Constant;
 import com.testikon.model.JsonPlaceHolderModel;
 import com.testikon.service.ReadService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
-@RestController
+@Controller
 @RequestMapping("/")
 public class MainController {
 
@@ -24,8 +24,9 @@ public class MainController {
     }
 
     @GetMapping("/visualize")
-    public String visualizeJson() {
-        return readService.getTitlesAndBodiesAsHtml(Constant.URL_JSON);
+    public String visualizeJson(Model model) {
+        String htmlContent = readService.getTitlesAndBodiesAsHtml(Constant.URL_JSON);
+        model.addAttribute("htmlContent", htmlContent);
+        return "visualize";
     }
-
 }
